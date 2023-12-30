c9d20d5a99964efd3c41f8306ccd821a665b2e07
fernando-salviati <fernando@cosette.com.au>
2023-12-31 10:07:17 +1100

    initial commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+const questions = [
+  {
+    id: 1,
+    title:
+      "Is it necessary to provide my phone number for account verification?",
+    info: "To enhance the security of your account, we require phone number verification. This ensures that you are the rightful owner of the account and allows us to send important notifications and alerts to you via SMS.",
+  },
+  {
+    id: 2,
+    title: "How can I update the profile picture on my account?",
+    info: "Updating your profile picture is simple. Navigate to the 'Profile Settings' section, where you'll find an option to upload a new image. Choose a clear and recognizable photo to personalize your account and make it easily identifiable.",
+  },
+  {
+    id: 3,
+    title: "What are the security features of the Two-Factor Authentication?",
+    info: "Two-Factor Authentication adds an extra layer of protection to your account. When enabled, you'll receive a unique code on your registered device, ensuring that only you can access your account even if your password is compromised.",
+  },
+  {
+    id: 4,
+    title: "Can I link multiple bank accounts to my profile?",
+    info: "Yes, our platform allows you to link multiple bank accounts for your convenience. This feature is particularly useful if you manage finances across different institutions. You can easily switch between accounts within your profile settings.",
+  },
+  {
+    id: 5,
+    title: "How long does it take to process a password reset request?",
+    info: "Upon submitting a password reset request, you will receive an email with further instructions. The processing time typically takes a few minutes, but it may vary depending on network conditions. Check your inbox, including the spam folder, for the reset link.",
+  },
+];
+
+export default questions;
